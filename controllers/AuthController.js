5876e7f1b946bf90358acb8e52a3aa6bdf7c0853
Jaafar Mortada <mortada9190@gmail.com>
2024-06-16 20:31:24 +0300

    update users login timestamp

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,33 +2,33 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/UserModel");
 
 exports.login = async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
+	try {
+		const { username, password } = req.body;
 
-    if (!(username && password)) {
-      res.status(400).send("All fields are required!");
-    }
-    let results = await User.getByUsernameAndPassword(username, password);
-    if (results) {
-      const token = jwt.sign(
-        {
-          user_id: results.user_id.toString(),
-          username,
-          user_type: results.user_type,
-        },
-        "$3a#_cJDUV-$QsRewWXcyH-Xdji8#%^$*(_ZkfNdI@#!D-Nv0E_M3a"
-      );
+		if (!(username && password)) {
+			res.status(400).send("All fields are required!");
+		}
+		let results = await User.getByUsernameAndPassword(username, password);
+		if (results) {
+			const token = jwt.sign(
+				{
+					user_id: results.user_id.toString(),
+					username,
+					user_type: results.user_type,
+				},
+				"$3a#_cJDUV-$QsRewWXcyH-Xdji8#%^$*(_ZkfNdI@#!D-Nv0E_M3a"
+			);
 
-      const user = {
-        username: username,
-        user_type: results.user_type,
-        token: token,
-      };
-      res.status(200).send(user);
-    } else {
-      res.status(401).send("Incorrect username or password!");
-    }
-  } catch (error) {
-    next(error);
-  }
+			const user = {
+				username: username,
+				user_type: results.user_type,
+				token: token,
+			};
+			res.status(200).send(user);
+		} else {
+			res.status(401).send("Incorrect username or password!");
+		}
+	} catch (error) {
+		next(error);
+	}
 };
--- models/UserModel.js
+++ models/UserModel.js
@@ -2,89 +2,99 @@ const pool = require("../config/database");
 const bcrypt = require("bcryptjs");
 
 class User {
-  // get all users except admin
-  static async getAll() {
-    const [rows] = await pool.query(
-      `SELECT user_id, username, first_name, last_name, view_stock, view_reports, delete_invoice, modify_customers FROM users WHERE user_type = 'user' AND is_deleted = 0`
-    );
-    return rows;
-  }
+	// get all users except admin
+	static async getAll() {
+		const [rows] = await pool.query(
+			`SELECT user_id, username, first_name, last_name, view_stock, view_reports, delete_invoice, modify_customers FROM users WHERE user_type = 'user' AND is_deleted = 0`
+		);
+		return rows;
+	}
 
-  // get by id
-  static async getById(id) {
-    const [rows] = await pool.query(
-      `SELECT user_id, username, first_name, last_name, view_stock, view_reports, delete_invoice, modify_customers FROM users WHERE user_id = ?`,
-      id
-    );
-    return rows;
-  }
+	// get by id
+	static async getById(id) {
+		const [rows] = await pool.query(
+			`SELECT user_id, username, first_name, last_name, view_stock, view_reports, delete_invoice, modify_customers FROM users WHERE user_id = ?`,
+			id
+		);
+		return rows;
+	}
 
-  // validate by username
-  static async getByUsername(username) {
-    const [rows] = await pool.query(
-      `SELECT username FROM users WHERE username = ? AND is_deleted = 0`,
-      username
-    );
-    return rows;
-  }
+	// validate by username
+	static async getByUsername(username) {
+		const [rows] = await pool.query(
+			`SELECT username FROM users WHERE username = ? AND is_deleted = 0`,
+			username
+		);
+		return rows;
+	}
 
-  // validate by id and username
-  static async getByIdAndUsername(id, username) {
-    const [rows] = await pool.query(
-      `SELECT username FROM users WHERE user_id != ?  AND username = ? AND is_deleted = 0`,
-      [id, username]
-    );
-    return rows;
-  }
+	// validate by id and username
+	static async getByIdAndUsername(id, username) {
+		const [rows] = await pool.query(
+			`SELECT username FROM users WHERE user_id != ?  AND username = ? AND is_deleted = 0`,
+			[id, username]
+		);
+		return rows;
+	}
 
-  // validate by username and password
-  static async getByUsernameAndPassword(username, password) {
-    const [[rows]] = await pool.query(
-      `SELECT * FROM users WHERE username = ? AND is_deleted = 0`,
-      [username]
-    );
-    if (!rows) return null;
-    const verified = bcrypt.compareSync(password, rows.password);
-    return verified ? rows : null;
-  }
+	// validate by username and password
+	static async getByUsernameAndPassword(username, password) {
+		const [[rows]] = await pool.query(
+			`SELECT * FROM users WHERE username = ? AND is_deleted = 0`,
+			[username]
+		);
 
-  // validate password by user_id
-  static async getByPassword(id) {
-    const [rows] = await pool.query(
-      `SELECT password FROM users WHERE user_id = ?`,
-      id
-    );
-    return rows;
-  }
+		if (!rows) return null;
 
-  // create user
-  static async create(user) {
-    user.password = bcrypt.hashSync(user.password, 10);
-    const [rows] = await pool.query(`INSERT INTO users SET ?`, user);
-    return rows;
-  }
+		await pool.query(
+			`UPDATE users SET last_login = CURRENT_TIMESTAMP WHERE username = ?`,
+			username
+		);
 
-  // update user
-  static async update(user) {
-    await pool.query(`UPDATE users SET ? WHERE user_id = ?`, [
-      user,
-      user.user_id,
-    ]);
-  }
+		const verified = bcrypt.compareSync(password, rows.password);
+		return verified ? rows : null;
+	}
 
-  // update password
-  static async updatePassword(user_id, password) {
-    const hashed_password = bcrypt.hashSync(password, 10);
-    await pool.query(`UPDATE users SET password = ? WHERE user_id = ?`, [
-      hashed_password,
-      user_id,
-    ]);
-  }
+	// validate password by user_id
+	static async getByPassword(id) {
+		const [rows] = await pool.query(
+			`SELECT password FROM users WHERE user_id = ?`,
+			id
+		);
+		return rows;
+	}
 
-  // delete user
-  static async delete(id) {
-    await pool.query(`UPDATE users SET is_deleted = 1 WHERE user_id = ?`, id);
-  }
+	// create user
+	static async create(user) {
+		user.password = bcrypt.hashSync(user.password, 10);
+		const [rows] = await pool.query(`INSERT INTO users SET ?`, user);
+		return rows;
+	}
+
+	// update user
+	static async update(user) {
+		await pool.query(`UPDATE users SET ? WHERE user_id = ?`, [
+			user,
+			user.user_id,
+		]);
+	}
+
+	// update password
+	static async updatePassword(user_id, password) {
+		const hashed_password = bcrypt.hashSync(password, 10);
+		await pool.query(`UPDATE users SET password = ? WHERE user_id = ?`, [
+			hashed_password,
+			user_id,
+		]);
+	}
+
+	// delete user
+	static async delete(id) {
+		await pool.query(
+			`UPDATE users SET is_deleted = 1 WHERE user_id = ?`,
+			id
+		);
+	}
 }
 
 module.exports = User;
