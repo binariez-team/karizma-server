137f5250caa4bbbfd8293cbd78b862b66d17a7c1
Jaafar Mortada <mortada9190@gmail.com>
2024-06-16 21:03:13 +0300

    update errors path

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const logFilePath = path.join(__dirname, "..", "error.log");
+const logFilePath = path.join(__dirname, "..", "errors", "error.log");
 
 const errorHandler = (err, req, res, next) => {
 	console.error(err);
