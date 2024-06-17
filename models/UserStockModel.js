2851d2a361e0ac91b808b270f51d1e8e278dfc50
abbass-haidar <103500956+abbass-haidar@users.noreply.github.com>
2024-06-18 01:23:33 +0300

    Update UserStockModel.js

--- models/UserStockModel.js
+++ models/UserStockModel.js
@@ -1,8 +1,8 @@
 const pool = require("../config/database");
 
 class UserProduct {
-	static async getAll(user_id) {
-		const query = `SELECT
+  static async getAll(user_id) {
+    const query = `SELECT
             C.category_name,
 			B.brand_name,
             P.*,
@@ -23,18 +23,18 @@ class UserProduct {
             WHERE P.is_deleted = 0 AND user_id_fk = ?
             ORDER BY P.product_id ASC;`;
 
-		const [result] = await pool.query(query, [
-			user_id,
-			user_id,
-			user_id,
-			user_id,
-			user_id,
-		]);
-		return result;
-	}
+    const [result] = await pool.query(query, [
+      user_id,
+      user_id,
+      user_id,
+      user_id,
+      user_id,
+    ]);
+    return result;
+  }
 
-	static async getById(user_id, product_id) {
-		const query = `SELECT
+  static async getById(user_id, product_id) {
+    const query = `SELECT
             C.category_name,
       		B.brand_name,
             P.*,
@@ -55,32 +55,33 @@ class UserProduct {
 			INNER JOIN inventory I ON P.product_id = I.product_id_fk
 			LEFT JOIN products_categories C ON P.category_id_fk = C.category_id
 			LEFT JOIN products_brands B ON P.brand_id_fk = B.brand_id
-			WHERE P.is_deleted = 0 AND P.product_id = ?;`;
+			WHERE P.is_deleted = 0 AND user_id_fk = ? AND P.product_id = ?;`;
 
-		const [result] = await pool.query(query, [
-			user_id,
-			user_id,
-			user_id,
-			user_id,
-			product_id,
-		]);
-		return result;
-	}
-	static async update(user_id, product_id, prices) {
-		const query = `UPDATE inventory
+    const [result] = await pool.query(query, [
+      user_id,
+      user_id,
+      user_id,
+      user_id,
+      user_id,
+      product_id,
+    ]);
+    return result;
+  }
+  static async update(user_id, product_id, prices) {
+    const query = `UPDATE inventory
             SET grandwhole_price_usd = ?,
             whole_price_usd = ?,
             unit_price_usd = ?
             WHERE product_id_fk = ? AND user_id_fk = ?;`;
 
-		await pool.query(query, [
-			prices.grandwhole_price_usd,
-			prices.whole_price_usd,
-			prices.unit_price_usd,
-			product_id,
-			user_id,
-		]);
-	}
+    await pool.query(query, [
+      prices.grandwhole_price_usd,
+      prices.whole_price_usd,
+      prices.unit_price_usd,
+      product_id,
+      user_id,
+    ]);
+  }
 }
 
 module.exports = UserProduct;
