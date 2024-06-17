75210a1f2352003db71a771f6f45d50bbb26d18e
Jaafar Mortada <mortada9190@gmail.com>
2024-06-17 13:04:05 +0300

    updates

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,14 +1,30 @@
 const fs = require("fs");
 const path = require("path");
 
-const logFilePath = path.join(__dirname, "..", "errors", "error.log");
+// Function to ensure the error directory exists
+const ensureErrorDirectoryExists = (directory) => {
+	if (!fs.existsSync(directory)) {
+		fs.mkdirSync(directory, { recursive: true });
+	}
+};
 
 const errorHandler = (err, req, res, next) => {
-	console.error(err);
+	console.log(err);
+
+	const errorDirectory = path.join(__dirname, "../errors");
+	ensureErrorDirectoryExists(errorDirectory);
+
+	const errorLogPath = path.join(errorDirectory, "error.log");
+
+	if (!fs.existsSync("../errors")) {
+		console.log("called");
+		fs.mkdirSync("../errors", { recursive: true }); // Create directory if it doesn't exist
+		fs.writeFileSync(logFilePath);
+	}
 
 	const timestamp = new Date().toISOString();
 	const logMessage = `${timestamp} [ERROR]: ${err}\n`;
-	fs.appendFile(logFilePath, logMessage, (err) => {
+	fs.appendFile(errorLogPath, logMessage, (err) => {
 		if (err) {
 			console.error("Failed to write to log file:", err);
 		}
