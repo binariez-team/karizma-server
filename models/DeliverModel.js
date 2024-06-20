42027a3ece355e30f61f49f0f10568d499751241
Jaafar Mortada <mortada9190@gmail.com>
2024-06-20 17:06:35 +0300

    completed deliver

--- models/DeliverModel.js
+++ models/DeliverModel.js
@@ -2,7 +2,7 @@ const pool = require("../config/database");
 const moment = require("moment");
 
 class DeliverInvoice {
-	static async create(order, invoice, user) {
+	static async create(order, items, user) {
 		const connection = await pool.getConnection();
 		try {
 			await connection.beginTransaction();
@@ -21,20 +21,60 @@ class DeliverInvoice {
 			// inserted order ID
 			let order_id = result.insertId;
 
-			let order_items = Array.from(invoice).map(function (item) {
-				return [
-					order_id,
-					item.product_id,
-					item.quantity,
-					item.unit_cost_usd,
-				];
-			});
+			//	loop of each invoice item record
+			for (const record of items) {
+				delete record.product_name;
 
-			await connection.query(
-				`INSERT INTO deliver_order_items (order_id_fk, product_id_fk, quantity, unit_price) VALUES ?`,
-				[order_items]
-			);
+				// insert into order items
+				await connection.query(
+					`INSERT INTO deliver_order_items SET ?`,
+					{ ...record, order_id_fk: order_id }
+				);
 
+				// check if inventory records has previously inserted
+				let [[inserted]] = await connection.query(
+					`SELECT * FROM inventory WHERE product_id_fk = ? AND user_id_fk = ? AND is_deleted = 0;`,
+					[record.product_id, order.user_id_fk]
+				);
+
+				// if records have not been inserted before
+				if (!inserted) {
+					// prepare default inventory record prices (created by admin)
+					let [[rows]] = await connection.query(
+						`SELECT * FROM inventory WHERE user_id_fk = ? AND product_id_fk = ?;`,
+						[order.admin_id_fk, record.product_id]
+					);
+					let {
+						grandwhole_price_usd,
+						whole_price_usd,
+						unit_price_usd,
+					} = rows;
+					const inventoryRecord = {
+						product_id_fk: record.product_id,
+						user_id_fk: order.user_id_fk,
+						grandwhole_price_usd: grandwhole_price_usd,
+						whole_price_usd: whole_price_usd,
+						unit_price_usd: unit_price_usd,
+					};
+					// insert a new inventory record with default prices
+					await connection.query(
+						`INSERT INTO inventory SET ?`,
+						inventoryRecord
+					);
+				}
+
+				// add admin record to inventory transactions
+				await connection.query(
+					`INSERT INTO inventory_transactions (product_id_fk, user_id_fk, quantity, transaction_type) VALUES (${record.product_id}, ${order.admin_id_fk}, ${record.quantity}, 'DELIVER');`
+				);
+
+				// add user record to inventory transactions
+				await connection.query(
+					`INSERT INTO inventory_transactions (product_id_fk, user_id_fk, quantity, transaction_type) VALUES (${record.product_id}, ${order.user_id_fk}, ${record.quantity}, 'DELIVER');`
+				);
+			}
+
+			// after successfull
 			await connection.commit();
 		} catch (error) {
 			await connection.rollback();
