4367d9864bb0fc0ce20a9bd6b7fa47d7bcdd50f0
Jaafar Mortada <mortada9190@gmail.com>
2024-06-20 18:58:39 +0300

    updates socket delivery

--- controllers/DeliverController.js
+++ controllers/DeliverController.js
@@ -1,11 +1,15 @@
 const DeliverInvoice = require("../models/DeliverModel");
+const User = require("../models/UserModel");
 
 exports.createDeliverInvoice = async (req, res, next) => {
 	try {
-		const user = req.user;
+		const io = req.io;
+		const admin = req.user;
 		const { order, items } = req.body;
 
-		await DeliverInvoice.create(order, items, user);
+		await DeliverInvoice.create(order, items, admin);
+		const [user] = await User.getById(order.user_id_fk);
+		io.emit("deliverAdded", user.username);
 		res.status(201).send({
 			message: "Order created successfully!",
 		});
