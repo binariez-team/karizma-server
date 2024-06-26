3fc178a591731f6c092eacba6641445c2a84b28d
Jaafar Mortada <mortada9190@gmail.com>
2024-06-26 18:15:25 +0300

    fix silly bug

--- models/UserHistoryModel.js
+++ models/UserHistoryModel.js
@@ -12,7 +12,7 @@ class UserHistory {
             	INNER JOIN deliver_order_items M ON O.order_id = M.order_id_fk
 				INNER JOIN products S ON S.product_id = M.product_id
 				WHERE O.is_deleted = 0
-				AND o.user_id_fk = ? `;
+				AND O.user_id_fk = ? `;
 		const params = [user_id];
 		if (criteria.invoice_number) {
 			sql += ` AND O.invoice_number = ?`;
