c693ab454875c01d20f2c510e66815273c0a413e
abbass-haidar <103500956+abbass-haidar@users.noreply.github.com>
2024-07-23 21:21:48 +0300

    change report to handle returns
    subtract returns from costs and total and profit

--- models/ReportModel.js
+++ models/ReportModel.js
@@ -2,16 +2,47 @@ const pool = require("../config/database");
 
 class ReportModel {
 	static async getTotalSales(user_id, startDate, endDate) {
-		let query = `SELECT
-        COALESCE(SUM(total_amount),0) AS totalSale,
-        COALESCE(SUM(total_cost), 0) AS totalCost,
-        COALESCE(SUM(total_amount - total_cost),0) AS grossProfit
-        FROM sales_orders
-        WHERE DATE(order_datetime) >= ?
-        AND DATE(order_datetime) <= ?
-        AND user_id = ?
-        AND is_deleted = 0`;
-		let [[result]] = await pool.query(query, [startDate, endDate, user_id]);
+		let query = ` WITH sales_summary AS (
+            SELECT
+                COALESCE(SUM(total_amount), 0) AS totalSale,
+                COALESCE(SUM(total_cost), 0) AS totalCost,
+                COALESCE(SUM(total_amount - total_cost), 0) AS grossProfit
+            FROM
+                sales_orders
+            WHERE
+                DATE(order_datetime) >= ?
+                AND DATE(order_datetime) <= ?
+                AND user_id = ?
+                AND is_deleted = 0
+        ),
+        return_summary AS (
+            SELECT
+                COALESCE(SUM(total_amount), 0) AS totalReturn,
+                COALESCE(SUM(total_cost), 0) AS totalReturnCost,
+                COALESCE(SUM(total_amount - total_cost), 0) AS grossReturn
+            FROM
+                return_orders
+            WHERE
+                DATE(order_datetime) >= ?
+                AND DATE(order_datetime) <= ?
+                AND user_id = ?
+                AND is_deleted = 0
+        )
+        SELECT
+            (sales.totalSale - returns.totalReturn) AS totalSale,
+            (sales.totalCost - returns.totalReturnCost) AS totalCost,
+            (sales.grossProfit - returns.grossReturn) AS grossProfit
+        FROM
+            sales_summary sales
+            CROSS JOIN return_summary returns;`;
+		let [[result]] = await pool.query(query, [
+			startDate,
+			endDate,
+			user_id,
+			startDate,
+			endDate,
+			user_id,
+		]);
 		return result;
 	}
 
