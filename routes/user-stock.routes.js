bdc79b2e7894d5c5f5052882463db41c894d41dd
Jaafar Mortada <mortada9190@gmail.com>
2025-10-31 10:02:14 +0200

    updates

--- routes/user-stock.routes.js
+++ routes/user-stock.routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const UserStockController = require("../controllers/UserStockController");
+const AdminStockController = require("../controllers/AdminStockController");
+
 router.post("/dispose", UserStockController.disposeProducts);
 router.put("/dispose", UserStockController.updateDispose);
 router.delete("/dispose/:id", UserStockController.deleteDispose);
@@ -9,5 +11,7 @@ router.delete("/dispose/:id", UserStockController.deleteDispose);
 router.get("/", UserStockController.getAllProducts);
 router.get("/:id", UserStockController.getProductById);
 router.put("/:id", UserStockController.updateProduct);
+
+router.post("/correction", AdminStockController.addStockCorrection);
 
 module.exports = router;
